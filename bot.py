7c118d6c0fc171dbdbb06b5bf3e7853650bcd6ce
dimka527 <edisontop443@gmail.com>
2026-08-03 00:09:29 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -141,6 +141,4 @@ def run():
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 10000)))
 
 if __name__ == '__main__':
-    bot.remove_webhook()
-    bot.set_webhook(url="https://rocket-osint.onrender.com/" + API_TOKEN)
-    run()
+    app.run()
